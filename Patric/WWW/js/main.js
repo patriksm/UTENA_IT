6e97c31279705a9ade60575eca2add7bb0f6faf7
PatriksM <pmorevs@gmail.com>
2026-02-13 16:04:19 +0200

    snake movement

--- Patric/WWW/js/main.js
+++ Patric/WWW/js/main.js
@@ -12,8 +12,14 @@ var foodImg = new Image();
 foodImg.src = "img/mango.png";
 
 var food = {
-    x: Math.floor(17*Math.random() + 1)*BOX,
-    y: Math.floor(15*Math.random() + 3)*BOX
+    x: Math.floor(17 * Math.random() + 1) * BOX,
+    y: Math.floor(15 * Math.random() + 3) * BOX
+}
+
+var snake = [];
+snake[0] = {
+    x: 9 * BOX,
+    y: 10 * BOX
 }
 
 function drawMyGame() { //the function where all the game graphics will be drawn
@@ -21,6 +27,24 @@ function drawMyGame() { //the function where all the game graphics will be drawn
     ctx.drawImage(foodImg, food.x, food.y);
     //horizontal displacement: 1*BOX --> 17*BOX
     //vertical displacement: 3*BOX --> 17*BOX
+
+    for (let i = 0; i < snake.length; i++) {
+       ctx.fillRect(snake[i].x, snake[i].y, BOX, BOX); 
+    }
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    snakeX += BOX;
+
+    snake.pop();
+    
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
 
 let game = setInterval(drawMyGame, 100); //refresh function for the graphics
