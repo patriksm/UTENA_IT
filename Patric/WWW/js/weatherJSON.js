eba7c4a96741b99d4a45a70000d2570af9728199
PatriksM <pmorevs@gmail.com>
2026-03-05 17:03:06 +0200

    JSON half done

---
+++ Patric/WWW/js/weatherJSON.js
@@ -0,0 +1,24 @@
+var myBlock = document.getElementById("weather");
+
+var requestURL = `http://api.weatherapi.com/v1/current.json?key=279113872acc4f48a39115257240403&q=Utena&aqi=yes
+`;
+
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+
+request.onload = () => {
+    if (request.status === 200 && request.readyState === request.DONE) {
+        var myWeather = request.response;
+        displayWeather(myWeather);
+    }
+}
+
+request.send();
+
+function displayWeather(jsonOBJ) {
+    var myH1 = document.createElement('H1');
+    myH1.textContent = "Temperature in " + jsonOBJ['location'] + " is " + " C";
+    myBlock.appendChild(myH1)
+}
