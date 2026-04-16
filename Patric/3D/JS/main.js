33ea88bf4bed3616e3a49eba49f500fb4fdfd162
PatriksM <pmorevs@gmail.com>
2026-04-16 16:32:41 +0300

    floor and three walls

--- Patric/3D/JS/main.js
+++ Patric/3D/JS/main.js
@@ -2,26 +2,31 @@ let myWolrd = document.getElementById("world");
 
 let lvl_one_map = [
     { id: "floor", w: 1000, h: 1000, color: "grey", x: 0, y: 0, z: -100, rx: 0, ry: 90, rz: 0 },
-    {}
+    { id: "hinter wall", w: 1000, h: 200, color: "aquamarine", x: -500, y: 0, z: 0, rx: 0, ry: 0, rz: 0 },
+    { id: "right wall", w: 1000, h: 200, color: "#ff0086", x: 0, y: 500, z: 0, rx: 0, ry: 0, rz: 90 },
+    { id: "left wall", w: 1000, h: 200, color: "#17c97d", x: 0, y: -500, z: 0, rx: 0, ry: 0, rz: 90 }
 ];
 
 function drawSquares(map) {
-    let myNewElement = document.createElement("div");
-    myNewElement.id = map[0].id;
-    myNewElement.style.position = "absolute";
-    myNewElement.style.width = `${map[0].w}px`;
-    myNewElement.style.height = `${map[0].h}px`;
-    myNewElement.style.backgroundColor = map[0].color;
-    myNewElement.style.transform =
-        `translate3d(
-    ${myWolrd.clientWidth / 2 - map[0].w / 2 + map[0].y}px, 
-    ${myWolrd.clientHeight / 2 - map[0].h / 2 - map[0].z}px, 
-    ${map[0].x}px
+    for (let i = 0; i < map.length; i++) {
+        let myNewElement = document.createElement("div");
+        myNewElement.id = map[i].id;
+        myNewElement.style.position = "absolute";
+        myNewElement.style.width = `${map[i].w}px`;
+        myNewElement.style.height = `${map[i].h}px`;
+        myNewElement.style.backgroundColor = map[i].color;
+        myNewElement.style.transform =
+            `translate3d(
+    ${myWolrd.clientWidth / 2 - map[i].w / 2 + map[i].y}px, 
+    ${myWolrd.clientHeight / 2 - map[i].h / 2 - map[i].z}px, 
+    ${map[i].x}px
     ) 
-    rotateX(${map[0].ry}deg) 
-    rotateY(${map[0].rz}deg) 
-    rotateZ(${map[0].rx}deg)`;
-    myWolrd.appendChild(myNewElement);
+    rotateX(${map[i].ry}deg) 
+    rotateY(${map[i].rz}deg) 
+    rotateZ(${map[i].rx}deg)`;
+        myWolrd.appendChild(myNewElement);
+    }
+
 }
 
 drawSquares(lvl_one_map);
